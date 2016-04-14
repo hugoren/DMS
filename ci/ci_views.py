b675f3c24dd36fa300968e2dbccefd995d5731d3
Hugo <860166687@qq.com>
2016-04-14 22:39:19 +0800

    流式上传大文件

--- ci/ci_views.py
+++ ci/ci_views.py
@@ -5,6 +5,12 @@ from sso.models import Url
 # from ci.pull_package_version import ssh_client
 import pull_package_version
 import  threading
+from django.shortcuts import  render,render_to_response
+from  django import  forms
+from  django.http import  HttpResponse
+import  os
+import  requests
+from  models import  upload_log
 # Create your views here.
 
 
@@ -34,3 +40,33 @@ def ci(request):
         #  return  render_to_response('index.html',{'data':url_list})
 
         return  render(request,'ci/pull_package_version.html',{"Pack_List":pull_package_version.ssh_client("119.29.101.41","admin","y298FTgS8Y").ssh_exce_cmd()})
+
+#
+# def pack_upload(request):
+#     return render(request,'ci/pack_upload.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+
+        un = request.POST.get('username')
+        f = request.FILES.get('uploadfile')  #'uploadfile'与提交表单中input名一致，多个文件参见getlist()
+        path = "./00"
+        if not os.path.dirname(path):
+            os.makedirs(path)
+        filename = os.path.join('00',f.name)
+        local_file = file(filename,'wb+')
+        print un
+        for chunk in f.chunks():
+            local_file.write(chunk)
+        return  HttpResponse(u'{}保存好了'.format(filename))
+    return render_to_response('ci/pack_upload.html',{})
+
+# def download_file():
+#
+#
+# def upload_api(request):
+#     url = 'http://localhost:8000/upload_api'
+#     files = {'file':open('/Users/hugo/PycharmProjects/Dsso/ci/command_list','rb')}
+#     r = requests.post(url,files=files)
+#     print  "success"
+
