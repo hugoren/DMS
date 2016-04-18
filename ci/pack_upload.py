628719182613e4339be10dcbaadd687e0374ad58
Hugo <860166687@qq.com>
2016-04-18 14:17:58 +0800

    按包名路径递层目录存储

--- ci/pack_upload.py
+++ ci/pack_upload.py
@@ -19,14 +19,28 @@ def upload_file(request):
         # un = request.POST.get('username')
         #'uploadfile'与提交表单中input名一致，多个文件参见getlist()
         f = request.FILES.get('uploadfile')
-        path = "./00"
-        if not os.path.dirname(path):
-            os.makedirs(path)
-        filename = os.path.join('00',f.name)
-        local_file = file(filename,'wb+')
-        for chunk in f.chunks():
-            local_file.write(chunk)
-        return  HttpResponse(u'{}保存好了'.format(f.name))
-    return render_to_response('ci/pack_upload.html',{})
+        #根据文件名逐层判断文件夹是否存在
+        filename_list = str(f.name).split('_')
+        pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
+        os.chdir(pack_path)
+        if not os.path.exists(filename_list[0]):
+            os.makedirs(filename_list[0])
+        os.chdir(pack_path+'/'+filename_list[0])
+        if not os.path.exists(filename_list[1]):
+            os.makedirs(filename_list[1])
+        os.chdir(pack_path+'/'+filename_list[0]+'/'+filename_list[1])
+        if not os.path.exists(filename_list[2]):
+            os.makedirs(filename_list[2])
+        #流方式存包
+        filename = os.path.join(pack_path+'/'+filename_list[0]+'/'+filename_list[1]+'/'+filename_list[2],f.name)
+        try:
+            local_file = file(filename,'wb+')
+            for chunk in f.chunks():
+                local_file.write(chunk)
+            return  HttpResponse(u'{}上传成功'.format(f.name))
+        except Exception as e:
+
+            return  HttpResponse(u'{}上传失败%s'%e.message.format(f.name))
+        return render_to_response('ci/pack_upload.html',{})
 
 
--- ci/pack_upload_client.py
+++ ci/pack_upload_client.py
@@ -5,14 +5,19 @@ import  urllib2
 from  poster.encode import  multipart_encode
 from  poster.streaminghttp import register_openers
 import  sys
+import  os
 
 
 register_openers()
 
-parameter_app = sys.argv[1]
+#包路径
+parameter_path = sys.argv[1]
+#环境_应用_版本_.tar.gz
+parameter_app = sys.argv[2]
 
 def upload_file():
     url = 'http://localhost:8000/upload/'
+    os.chdir(parameter_path)
     datagen,headers = multipart_encode({"uploadfile":open(parameter_app,'rb')})
     request = urllib2.Request(url,datagen,headers)
     print  urllib2.urlopen(request).read()
