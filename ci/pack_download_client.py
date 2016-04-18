a1ddbf268421c269b55f9dc46a6177888536a731
Hugo <860166687@qq.com>
2016-04-18 15:15:29 +0800

    指定路径下载

--- ci/pack_download_client.py
+++ ci/pack_download_client.py
@@ -1,13 +1,19 @@
+#coding:utf-8
 import  requests
 import  sys
+import  os
 
-parameter_app = sys.argv[1]
+#存储路径
+download_dir = sys.argv[1]
+#下载包名称
+parameter_app = sys.argv[2]
 def download_file():
     url = 'http://localhost:8000/download_client/%s'%parameter_app
     r = requests.get(url)
-    with open("%s"%parameter_app,"wb") as code:
-        code.write(r.content)
-    print  "success"
+    os.chdir(download_dir)
+    with open("%s"%parameter_app,"wb") as pack_content:
+        pack_content.write(r.content)
+    return "success"
 
 if __name__ == '__main__':
     download_file()
