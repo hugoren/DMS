1188fb37772a5c7f9f4ca0d703436aca617fd158
Hugo <860166687@qq.com>
2016-04-18 21:22:56 +0800

    解决因时区影响时间显示的问题

--- ci/pack_update.py
+++ ci/pack_update.py
@@ -2,13 +2,19 @@
 import os
 import time
 import shutil
+import datetime
 from  django.http import  HttpResponse
 
 def pack_update(request,parameter1,parameter2):
 
-    format = '%Y-%m-%d-%H:%M:%S'
-    value = time.localtime(time.time())
-    dt = time.strftime(format, value)
+    # format = '%Y-%m-%d-%H:%M:%S'
+    # value = time.localtime(time.time())
+    # value = time.localtime()
+    # dt = time.strftime(format, value)
+    a = datetime.datetime.today()
+    o = datetime.timedelta(hours=8)
+    # print (a+o).strftime("%Y-%m-%d_%H:%M")
+    # dt = time.strftime("%Y-%m-%d_%H-%M",time.localtime(time.time()))
     filename_list = str(parameter1).split('_')
     print  filename_list
     pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
@@ -28,7 +34,7 @@ def pack_update(request,parameter1,parameter2):
         os.chdir(pack_path+'/'+filename_list[0]+'/'+filename_list[1]+'/'+parameter2)
         # os.renames(parameter1,"%s_%s_.tar.gz"%(filename_list[0],filename_list[1])
         # os.renames('06.tar.gz','07.tar.gz')
-        os.renames(parameter1,"%s_%s_%s_%s.tar.gz"%(filename_list[0],filename_list[1],parameter2,dt))
+        os.renames(parameter1,"%s_%s_%s_%s.tar.gz"%(filename_list[0],filename_list[1],parameter2,(a+o).strftime("%Y-%m-%d_%H:%M")))
     except Exception as e:
         return e.message
     return HttpResponse(u'{}变更成功%s'.format(parameter1))
