f0836cff3d072f7ded9e8800838c37ab8cdfe9b4
Hugo <860166687@qq.com>
2016-04-18 14:41:50 +0800

    根据包路径解释下载

--- ci/pack_download.py
+++ ci/pack_download.py
@@ -1,6 +1,7 @@
 #coding:utf-8'
 
 from django.http import StreamingHttpResponse
+import os
 from  django.http import  HttpResponse
 
 
@@ -9,6 +10,10 @@ def pack_download(request,parameter):
     # do something...
     print parameter
     def file_iterator(file_name, chunk_size=512):
+        dir_list = str(parameter).split('_')
+        print  dir_list
+        pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
+        os.chdir(pack_path+'/'+dir_list[0]+'/'+dir_list[1]+'/'+dir_list[2])
         with file(file_name,'rb') as f:
             while True:
                 c = f.read(chunk_size)
@@ -17,8 +22,8 @@ def pack_download(request,parameter):
                 else:
                     break
 
-    the_file_name = '/Users/hugo/PycharmProjects/Dsso/00/%s'%parameter
-    response = StreamingHttpResponse(file_iterator(the_file_name))
+    # the_file_name = '/Users/hugo/PycharmProjects/Dsso/00/%s'%parameter
+    response = StreamingHttpResponse(file_iterator(parameter))
     response['Content-Type'] = 'application/octet-stream'
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(parameter)
 
