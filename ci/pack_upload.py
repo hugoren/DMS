0ab782c8c7237c606f0d33239aa3c19dbdd04ede
hugoren <859883773@qq.com>
2016-04-21 12:04:23 +0800

    url?传参方式

--- ci/pack_upload.py
+++ ci/pack_upload.py
@@ -15,30 +15,36 @@ def upload_file(request):
         # un = request.POST.get('username')
         #'uploadfile'与提交表单中input名一致，多个文件参见getlist()
         f = request.FILES.get('uploadfile')
-        query = request.GET.get('q','')
-        print  query
+        filename_list = str(f.name).split('/')
+
+        #获取客户传过来三个app_name,app_version,app_package参数
+        app_name = request.GET.get('app_name','')
+        app_version = request.GET.get('app_version','')
+        app_package = str(request.GET.get('app_package','')).split('/')
+        print app_name
+        print app_version
+        print app_package
+        print '%s    %s   %s'%(app_name,app_version,app_package)
         #根据文件名逐层判断文件夹是否存在
-        filename_list = str(f.name).split('_')
+
+        fisrt_layer = 'snapshot'
         pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
         os.chdir(pack_path)
-        if not os.path.exists(filename_list[0]):
-            os.makedirs(filename_list[0])
-        os.chdir(pack_path+'/'+filename_list[0])
-        if not os.path.exists(filename_list[1]):
-            os.makedirs(filename_list[1])
-        os.chdir(pack_path+'/'+filename_list[0]+'/'+filename_list[1])
-        if not os.path.exists(filename_list[2]):
-            os.makedirs(filename_list[2])
+        if not os.path.exists(fisrt_layer):
+            os.makedirs(fisrt_layer)
+        os.chdir(pack_path+'/'+fisrt_layer)
+        if not os.path.exists(app_name):
+            os.makedirs(app_name)
         #流方式存包
-        filename = os.path.join(pack_path+'/'+filename_list[0]+'/'+filename_list[1]+'/'+filename_list[2],f.name)
+        filename = os.path.join(pack_path+'/'+fisrt_layer+'/',f.name)
         try:
             local_file = file(filename,'wb+')
             for chunk in f.chunks():
                 local_file.write(chunk)
-            return  HttpResponse(u'{}上传成功'.format(f.name))
+            return  HttpResponse(u'{}上传成功'.format(app_name))
         except Exception as e:
 
-            return  HttpResponse(u'{}上传失败%s'%e.message.format(f.name))
+            return  HttpResponse(u'{}上传失败%s'%e.message.format(app_name))
     return render_to_response('ci/pack_upload.html',{})
 
 
--- ci/pack_upload_client.py
+++ ci/pack_upload_client.py
@@ -12,18 +12,17 @@ register_openers()
 
 
 
-def upload_file(parameter_path,parameter_app):
+def upload_file(parameter_app,parameter_version,parameter_package):
     # url = 'http://119.29.104.171:10000/upload/'
-    url = 'http://localhost:8000/upload/?q=boss'
-    os.chdir(parameter_path)
-    datagen,headers = multipart_encode({"uploadfile":open(parameter_app,'rb')})
+    url = 'http://localhost:8000/upload/?app_name=%s&app_version=%s&app_pacakage=%s'%(parameter_app,parameter_version,parameter_package)
+    # os.chdir(parameter_path)
+    datagen,headers = multipart_encode({"uploadfile":open(parameter_package,'rb')})
     request = urllib2.Request(url,datagen,headers)
     print  urllib2.urlopen(request).read()
 
 
 if __name__ == '__main__':
-    #包路径
-    parameter_path = sys.argv[1]
-    #环境_应用_版本_.tar.gz
-    parameter_app = sys.argv[2]
-    upload_file(parameter_path,parameter_app)
+    parameter_app = sys.argv[1]
+    parameter_version = sys.argv[2]
+    parameter_package = sys.argv[3]
+    upload_file(parameter_app,parameter_version,parameter_package)
