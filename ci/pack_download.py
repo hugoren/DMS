e492fbc4fa3954d5cd09cce803584c0e483b0e9e
hugoren <859883773@qq.com>
2016-04-22 11:26:06 +0800

    下载时默认最新版本

--- ci/pack_download.py
+++ ci/pack_download.py
@@ -2,19 +2,43 @@
 
 from django.http import StreamingHttpResponse
 import os
+import time
+import stat
 
 
-#web版下载
+#
+
+
 def pack_download(request):
 
     app_flag = request.GET.get('app_flag','')
     app_name = request.GET.get('app_name','')
     app_package = request.GET.get('app_package','')
+    pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
+    DIR = pack_path+'/'+app_flag+'/'+app_name
+
+    def pack_compare(x, y):
+        stat_x = os.stat(DIR + "/" + x)
+        stat_y = os.stat(DIR + "/" + y)
+        if stat_x.st_ctime < stat_y.st_ctime:
+            return -1
+        elif stat_x.st_ctime > stat_y.st_ctime:
+            return 1
+        else:
+            return 0
+    # package_list = os.listdir(DIR)
+    # package_latest = package_list.sort(pack_compare)
+    # # file_name = package_latest[-1]
+    # print package_latest
+
     def file_iterator(file_name, chunk_size=512):
-        print  app_package
-        pack_path = '/Users/hugo/PycharmProjects/Dsso/00'
-        os.chdir(pack_path+'/'+app_flag+'/'+app_name)
-        with file(app_package,'rb') as f:
+
+        os.chdir(DIR)
+        if file_name == 'latest':
+            package_list = os.listdir(DIR)
+            package_list.sort(pack_compare)
+            file_name = package_list[-1]
+        with file(file_name,'rb') as f:
             while True:
                 c = f.read(chunk_size)
                 if c:
@@ -27,7 +51,4 @@ def pack_download(request):
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(app_package)
 
     return response
-
-
-
 
