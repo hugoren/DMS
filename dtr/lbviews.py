66983d91b7f9a2c19d7e2aa6dd24bda927b9fc8b
hugoren <859883773@qq.com>
2016-04-26 10:18:13 +0800

    整合upstream的增删查改等功能

--- dtr/lbviews.py
+++ dtr/lbviews.py
@@ -7,23 +7,101 @@ from django.http import  HttpResponse
 
 class lb():
 
-    def __init__(self,ip,port,weight,max_fails,fail_timeout):
-        self.ip = ip
-        self.port = port
-        self.weight = weight
-        self.max_fails = max_fails
-        self.fail_timeout = fail_timeout
+    # def __init__(self,ip,port,weight,max_fails,fail_timeout,upstream_zone):
+    #     self.ip = ip
+    #     self.port = port
+    #     self.weight = weight
+    #     self.max_fails = max_fails
+    #     self.fail_timeout = fail_timeout
+    #     self.upsteam_zone = upstream_zone
 
-    def lb_list(request,self):
-        url = "http://203.195.140.214:20000//dynamic?upstream=zone_for_backends"
+    ip = "203.195.140.214"
+    port = 20000
+    weight = 10
+    max_fails = 10
+    fail_timeout = "10min"
+    upsteam_zone = "zone_for_backends"
+
+    #获取列表
+    def lb_list(request):
+        url = "http://%s:%s//dynamic?upstream=%s"%(lb.ip,lb.port,lb.upsteam_zone)
         try:
             r = requests.get(url)
             print r.content
-            return render(request,'dtr/lbviews.html',{'pack_list':r.content})
         except Exception as e:
-            e.message
+            print e.message
+
+    #获取upstream详细的参数信息
+    def lb_verbose(request):
+        url = "http://%s:%s//dynamic?upstream=%s&verbose="%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print  e.message
+
+    #更新upstream里的servers参数
+    def lb_update_parameters(request):
+        url = "http://%s:%s//dynamic?upstream=%s&server=127.0.0.1:30001&weight=10&max_fails=5&fail_timeout=5"%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print e.message
+
+    #设置servers_关
+    def lb_down(request):
+        url = "http://%s:%s//dynamic?upstream=%s&server=127.0.0.1:30001&down="%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print e.message
+
+
+    #设置servers_开
+    def lb_up(request):
+        url = "http://%s:%s//dynamic?upstream=%s&server=127.0.0.1:30002&up="%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print e.message
+
+    #设置servers_添加
+    def lb_add(request):
+        url = "http://%s:%s//dynamic?upstream=%s&add=&server=127.0.0.1:30004"%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print e.message
+
+
+    #设置servers_删除
+    def lb_remove(request):
+        url = "http://%s:%s//dynamic?upstream=%s&remove=&server=127.0.0.1:30004"%(lb.ip,lb.port,lb.upsteam_zone)
+        try:
+            r = requests.get(url)
+            if r.status_code == 200:
+                print "删除成功！"
+                print r.content
+            elif r.status_code == 400:
+                print  "该servers不存！"
+            else:
+                print "删除不成功，请检查其它原因！"
+
+        except Exception as e:
+            print e.message
+
 
 
 if __name__ == '__main__':
 
-    lb('203.195.140.214','30000','10','10min','00').view_list()
+    # lb().lb_list()
+    # lb().lb_verbose()
+    # lb().lb_update_parameters()
+    # lb().lb_down()
+    # lb().lb_up()
+    # lb().lb_add()
+    lb().lb_remove()
