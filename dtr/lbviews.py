94d05576098a3da06a9c4ad453cd0c8fe0e5ed07
hugoren <859883773@qq.com>
2016-04-27 09:55:00 +0800

    adjust

--- dtr/lbviews.py
+++ dtr/lbviews.py
@@ -20,9 +20,13 @@ fail_timeout = "10min"
 upsteam_zone = "zone_for_backends"
 upstream_conf = "../conf/upstream.conf"
 
-upstream_dict = {"A_running":["127.0.0.1:30000","127.0.0.1:30001"],"B_running":["127.0.0.1:30000","127.0.0.1:30001"],"C_standy":'["127.0.0.1:300,04","127.0.0.1:30005"]'}
-B_running = ["1270.0.1:30002","127.0.0.1:30003"]
-C_standy = ["127.0.0.1:300,04","127.0.0.1:30005"]
+upstream_dict = {"a_g_running":["127.0.0.1:30000","127.0.0.1:30001"],\
+                 "b_g_running":["127.0.0.1:30002","127.0.0.1:30003"],\
+                 "c_g_standy":["127.0.0.1:30004","127.0.0.1:30005"],\
+                 "a1_s_running":["127.0.0.1:30000"],"a2_s_running":["127.0.0.1:30001"],\
+                 "b1_s_running":["127.0.0.1:30002"],"b2_s_running":["127.0.0.1:30003"],\
+                 "c1_s_running":["127.0.0.1:30004"],"c2_s_running":["127.0.0.1:30005"]}
+
 
 # 读取配置文件
 def read_conf():
@@ -58,16 +62,15 @@ def read_argparse():
 def lb_list():
     url = "http://%s:%s//dynamic?upstream=%s"%(ip,port,upsteam_zone)
     try:
-        # r = requests.get(url)
-        t = ["11","2"]
-        # print r.content
-        return render(request,'dtr/lbviews.html',{"upstream_list":t})
+        r = requests.get(url)
+        print r.content
     except Exception as e:
         print e.message
+
 def t(request):
     return  render(request,'dtr/lbviews.html')
 
-#获取upstream详细的参数信息
+#list___获取upstream详细的参数信息
 def lb_verbose(request):
     url = "http://%s:%s//dynamic?upstream=%s&verbose="%(ip,port,upsteam_zone)
     try:
@@ -76,7 +79,7 @@ def lb_verbose(request):
     except Exception as e:
         print  e.message
 
-#更新upstream里的servers参数
+#update___更新upstream里的servers参数
 def lb_update_parameters(request):
     url = "http://%s:%s//dynamic?upstream=%s&server=127.0.0.1:30001&weight=10&max_fails=5&fail_timeout=5"%(ip,port,upsteam_zone)
     try:
@@ -85,26 +88,9 @@ def lb_update_parameters(request):
     except Exception as e:
         print e.message
 
-#设置servers_关
-def lb_down(upstream_servers):
-    # server_list = ["127.0.0.1:30000","127.0.0.1:30001"]
-    # A_running = ["127.0.0.1:30000","127.0.0.1:30001"]
-    server_list = upstream_dict[upstream_servers]
-    for s in server_list:
-        # url = "http://%s:%s//dynamic?upstream=%s&server=%s&down="%(ip,port,upsteam_zone,s)
-        # print s
-        try:
-            # r = requests.get(url)
-            # print r.content
-            print s
-        except Exception as e:
-            print e.message
-
-
-#设置servers_开
-def lb_up(request,upstream_list):
-    # server_list = ["127.0.0.1:30000","127.0.0.1:30001"]
-    for s in upstream_list:
+#up___servers_开
+def lb_up(upstream_servers):
+    for s in upstream_dict[upstream_servers]:
         url = "http://%s:%s//dynamic?upstream=%s&server=%s&up="%(ip,port,upsteam_zone,s)
         try:
             r = requests.get(url)
@@ -112,7 +98,24 @@ def lb_up(request,upstream_list):
         except Exception as e:
             print e.message
 
-#设置servers_添加
+#down___servers_关
+def lb_down(upstream_servers):
+    try:
+        server_list = upstream_dict[upstream_servers]
+    except Exception as e:
+        print "操作的组或单节点不存在"
+
+    for s in server_list:
+        url = "http://%s:%s//dynamic?upstream=%s&server=%s&down="%(ip,port,upsteam_zone,s)
+        # print s
+        try:
+            r = requests.get(url)
+            print r.content
+        except Exception as e:
+            print e.message
+
+
+#add___servers_添加
 def lb_add(request):
     url = "http://%s:%s//dynamic?upstream=%s&add=&server=127.0.0.1:30004"%(ip,port,upsteam_zone)
     try:
@@ -122,7 +125,7 @@ def lb_add(request):
         print e.message
 
 
-#设置servers_删除
+#delete__servers_删除
 def lb_remove(request):
     url = "http://%s:%s//dynamic?upstream=%s&remove=&server=%s"%(ip,port,upsteam_zone,"127.0.0.1:30001")
     try:
@@ -137,103 +140,6 @@ def lb_remove(request):
 
     except Exception as e:
         print e.message
-
-
-# 读取配置文件
-def read_conf(object):
-    config = ConfigParser.ConfigParser()
-    config.read("/Users/hugo/PycharmProjects/Dsso/dtr/conf/upstream.conf")
-    all_config = {}
-
-    for section in config.sections():
-        all_config[section] = {}
-        for key, value in config.items(section):
-            all_config[section][key] = value
-
-    config = all_config["C_standy_servers"]
-    print config
-    print config["c_standy1"]
-
-
-#获取列表
-def lb_list():
-    url = "http://%s:%s//dynamic?upstream=%s"%(ip,port,upsteam_zone)
-    try:
-        r = requests.get(url)
-        print r.content
-        # return render(request,'dtr/lbviews.html')
-    except Exception as e:
-        print e.message
-
-#获取upstream详细的参数信息
-def lb_verbose():
-    url = "http://%s:%s//dynamic?upstream=%s&verbose="%(ip,port,upsteam_zone)
-    try:
-        r = requests.get(url)
-        print r.content
-    except Exception as e:
-        print  e.message
-
-#更新upstream里的servers参数
-def lb_update_parameters():
-    url = "http://%s:%s//dynamic?upstream=%s&server=127.0.0.1:30001&weight=10&max_fails=5&fail_timeout=5"%(ip,port,upsteam_zone)
-    try:
-        r = requests.get(url)
-        print r.content
-    except Exception as e:
-        print e.message
-
-#设置servers_关
-def lb_down(server_key):
-    # A_running = ["127.0.0.1:30000","127.0.0.1:30001"]
-    server_list = upstream_dict[server_key]
-    for s in server_list:
-        # url = "http://%s:%s//dynamic?upstream=%s&server=%s&down="%(ip,port,upsteam_zone,s)
-        try:
-            # r = requests.get(url)
-            # print r.content
-            print  s
-        except Exception as e:
-            print e.message
-
-
-#设置servers_开
-def lb_up(request):
-    server_list = ["127.0.0.1:30000","127.0.0.1:30001"]
-    for s in server_list:
-        url = "http://%s:%s//dynamic?upstream=%s&server=%s&up="%(ip,port,upsteam_zone,s)
-        try:
-            r = requests.get(url)
-            print r.content
-        except Exception as e:
-            print e.message
-
-#设置servers_添加
-def lb_add():
-    url = "http://%s:%s//dynamic?upstream=%s&add=&server=127.0.0.1:30004"%(ip,port,upsteam_zone)
-    try:
-        r = requests.get(url)
-        print r.content
-    except Exception as e:
-        print e.message
-
-
-#设置servers_删除
-def lb_remove():
-    url = "http://%s:%s//dynamic?upstream=%s&remove=&server=%s"%(ip,port,upsteam_zone,"127.0.0.1:30001")
-    try:
-        r = requests.get(url)
-        if r.status_code == 200:
-            print "删除成功！"
-            print r.content
-        elif r.status_code == 400:
-            print  "该servers不存！"
-        else:
-            print "删除不成功，请检查其它原因！"
-
-    except Exception as e:
-        print e.message
-
 
 
 if __name__ == '__main__':
@@ -257,7 +163,7 @@ if __name__ == '__main__':
         lb_down(sys.argv[2])
 
     elif actions == 'up':
-        lb_up(request)
+        lb_up(sys.argv[2])
     # args = read_argparse()
     # actions = args.app
     # print actions
