9c53ac4a60f5a4ff554e26af25aca1abc2ece928
hugoren <859883773@qq.com>
2016-04-29 16:22:46 +0800

    adjust

--- ci/pack_update_client.py
+++ ci/pack_update_client.py
@@ -8,7 +8,9 @@ def update_version(parameter_app,parameter_package):
     # url = 'http://119.29.104.171:10000/update/%s'%(parameter_packageName)
     #django传参一种方式
     # url = 'http://localhost:8000/update/%s'%(parameter_packageName)
-    url = 'http://localhost:8000/update/?app_name=%s&app_package=%s'%(parameter_app,parameter_package)
+    # url = 'http://localhost:8000/update/?app_name=%s&app_package=%s'%(parameter_app,parameter_package)
+    url = 'http://119.29.104.171:10000/update/?app_name=%s&app_package=%s'%(parameter_app,parameter_package)
+
     r = requests.get(url)
     if (r.status_code == 200):
         print "%s,包状态更更成功"%parameter_package
