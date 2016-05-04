a109be43eb941d057a951b0929e372a45dfcba32
hugoren <859883773@qq.com>
2016-05-04 21:43:34 +0800

    map多线程

---
+++ ci/map_dummy.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+import urllib2
+from multiprocessing.dummy import Pool as ThreadPool
+def map_dummy():
+    urls = [
+      'http://www.python.org',
+      'http://www.python.org/about/',
+      'http://www.onlamp.com/pub/a/python/2003/04/17/metaclasses.html',
+      'http://www.python.org/doc/',
+      'http://www.python.org/download/',
+      'http://www.python.org/getit/',
+      'http://www.python.org/community/',
+      'https://wiki.python.org/moin/',
+      'http://planet.python.org/',
+      'https://wiki.python.org/moin/LocalUserGroups',
+      'http://www.python.org/psf/',
+      'http://docs.python.org/devguide/',
+      'http://www.python.org/community/awards/'
+      # 等等...
+      ]
+
+    # 创建一个工作者线程池
+    pool = ThreadPool(4)
+    # 在各个线程中打开url，并返回结果
+    results = pool.map(urllib2.urlopen, urls)
+    #close the pool and wait for the work to finish
+    # 关闭线程池，等待工作结束
+    pool.close()
+    pool.join()
+    for s in results:
+        print  s
+
+map_dummy()
