901ca79d56be2e176d721b7f4da43eba1e4a7676
hugoren <859883773@qq.com>
2016-05-09 16:40:45 +0800

    函数式编程

--- ci/test.py
+++ ci/test.py
@@ -35,21 +35,53 @@
 #     print "inside aFunction()"
 #
 # aFunction
+#
+#
+# def makeHtmlTag(tag,*arg,**kwds):
+#     def real_decorator(fn):
+#         css_class = " class='{0}'".format(kwds["css_class"])\
+#         if "css_class" in kwds else ""
+#
+#         def wrapped(*args,**kwds):
+#             return "<"+tag+css_class+">" + fn(*args,**kwds)+ "</"+tag+">"
+#         return wrapped
+#     return real_decorator
+#
+#
+# @makeHtmlTag(tag="b",css_class="bold_css")
+# @makeHtmlTag(tag="i",css_class="italic_css")
+# def hello():
+#     return "hello world"
+# print hello()
 
 
-def makeHtmlTag(tag,*arg,**kwds):
-    def real_decorator(fn):
-        css_class = " class='{0}'".format(kwds["css_class"])\
-        if "css_class" in kwds else ""
+def t2():
+    name_len = map(len,["hao","xiang","ni"])
+    print name_len
 
-        def wrapped(*args,**kwds):
-            return "<"+tag+css_class+">" + fn(*args,**kwds)+ "</"+tag+">"
-        return wrapped
-    return real_decorator
+    name_list = ["hao","xiang","ni"]
+    for i in name_list:
+        print len(i)
+
+t2()
 
 
-@makeHtmlTag(tag="b",css_class="bold_css")
-@makeHtmlTag(tag="i",css_class="italic_css")
-def hello():
-    return "hello world"
-print hello()
+def toUpper(item):
+    return item.upper()
+upper_name = map(toUpper,["hao","xiang","ni"])
+print upper_name
+
+
+def t3():
+    squares = map(lambda i:i * i,range(9))
+    print squares
+t3()
+
+def t4():
+    num = [0,-1,-3,-5,-8,7,3,9,10]
+    positive_num = filter(lambda x:x>0,num)
+    print positive_num
+    average = reduce(lambda x,y:x+y,positive_num)/len(positive_num)
+    print average
+
+t4()
