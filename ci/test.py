dcd540361cdc246e8447af09e2d695e586f8fd7f
hugoren <859883773@qq.com>
2016-05-15 09:54:50 +0800

    浅拷贝，深拷贝

--- ci/test.py
+++ ci/test.py
@@ -55,58 +55,83 @@
 # print hello()
 
 
-def t2():
-    name_len = map(len,["hao","xiang","ni"])
-    print name_len
+# def t2():
+#     name_len = map(len,["hao","xiang","ni"])
+#     print name_len
+#
+#     name_list = ["hao","xiang","ni"]
+#     for i in name_list:
+#         print len(i)
+#
+# t2()
+#
+#
+# def toUpper(item):
+#     return item.upper()
+# upper_name = map(toUpper,["hao","xiang","ni"])
+# print upper_name
+#
+#
+# def t3():
+#     squares = map(lambda i:i * i,range(9))
+#     print squares
+# t3()
+#
+# def t4():
+#     num = [0,-1,-3,-5,-8,7,3,9,10]
+#     positive_num = filter(lambda x:x>0,num)
+#     print positive_num
+#     average = reduce(lambda x,y:x+y,positive_num)/len(positive_num)
+#     print average
+#
+# t4()
+#
+#
+#
+# from random import  random
+#
+# def move_cars(car_position):
+#     return map(lambda x:x+1 if random() > 0.3 else x,car_position)
+#
+# def output_car(car_position):
+#     return '-' * car_position
+#
+# def run_step_of_race(state):
+#     return {'time':state['time'] -1,'car_positions':move_cars(state['car_positions'])}
+#
+# def draw(state):
+#     print ''
+#     print '\n'.join(map(output_car,state['car_positions']))
+#
+# def race(state):
+#     draw(state)
+#     if state['time']:
+#         race(run_step_of_race(state))
+#
+# race({'time':5,'car_positions':[1,1,1]})
 
-    name_list = ["hao","xiang","ni"]
-    for i in name_list:
-        print len(i)
 
-t2()
+import copy
 
-
-def toUpper(item):
-    return item.upper()
-upper_name = map(toUpper,["hao","xiang","ni"])
-print upper_name
-
-
-def t3():
-    squares = map(lambda i:i * i,range(9))
-    print squares
-t3()
-
-def t4():
-    num = [0,-1,-3,-5,-8,7,3,9,10]
-    positive_num = filter(lambda x:x>0,num)
-    print positive_num
-    average = reduce(lambda x,y:x+y,positive_num)/len(positive_num)
-    print average
-
-t4()
+# n1 = 123
+# print  (id(n1))
+#
+#
+# n2 = n1
+# print (id(n2))
+#
+#
+# n3 = copy.copy(n1)
+# print (id(n3))
+#
+#
+# n4 = copy.deepcopy(n1)
+# print (id(n4))
 
 
 
-from random import  random
-
-def move_cars(car_position):
-    return map(lambda x:x+1 if random() > 0.3 else x,car_position)
-
-def output_car(car_position):
-    return '-' * car_position
-
-def run_step_of_race(state):
-    return {'time':state['time'] -1,'car_positions':move_cars(state['car_positions'])}
-
-def draw(state):
-    print ''
-    print '\n'.join(map(output_car,state['car_positions']))
-
-def race(state):
-    draw(state)
-    if state['time']:
-        race(run_step_of_race(state))
-
-race({'time':5,'car_positions':[1,1,1]})
-
+# n1 = {"k1":"nick","k2":123,"k3":["jenny",666]}
+# n2 = copy.deepcopy(n1)
+# n3 = copy.copy(n1)
+# print n2
+# print n3
